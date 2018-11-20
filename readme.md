8af646370cd55df9fe88b1ff065bb253e1b9aff2
soyeon <peach404@naver.com>
2018-11-20 14:00:04 +0900

    [ADD] 덮어쓰기방지하기

--- readme.md
+++ readme.md
@@ -19,5 +19,14 @@ Component 애노테이션이 숨어있으므로 빈으로 등록해준다.
 <img width="516" alt="2018-11-20 1 07 19" src="https://user-images.githubusercontent.com/38067653/48751035-a245dc80-ecc5-11e8-98e5-2fcb68b218c5.png">
 <img width="324" alt="2018-11-20 1 07 54" src="https://user-images.githubusercontent.com/38067653/48751048-affb6200-ecc5-11e8-9215-4caca4af0034.png">
 
+##덮어쓰기를 방지하기
+@ConditionalOnMissingBean 을 붙여주면 그 설정 파일을 사용하는 프로젝트에서 설정을 수정했다면 그 수정 파일이 적용된다.
+
+빈재정의로 수정하지 말고 설정 프로젝트에서 별도의 
+Hellotestpropertise 파일 을 생성해서 @ConfigurationProperties 애노테이션을 달고 gettersetter을 달아준다.
+application.properties 파일에 hellotest.name , hellotest.age 값 작성으로 빈 재정의 없이도 변경가능해진다.
+
+
+
 
 
--- src/main/java/me/soyeon/Application.java
+++ src/main/java/me/soyeon/Application.java
@@ -4,6 +4,7 @@ package me.soyeon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -12,7 +13,14 @@ public class Application {
         SpringApplication application = new SpringApplication(Application.class);
         application.setWebApplicationType(WebApplicationType.NONE);
         application.run(args);
-
-
     }
+
+    //빈 재정의 방식으로 수정
+//    @Bean
+//    public Hellotest hellotest(){
+//        Hellotest hellotest = new Hellotest();
+//        hellotest.setName("NEWPUPPY");
+//        hellotest.setAge(10);
+//        return  hellotest;
+//    }
 }
