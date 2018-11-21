4f00357edc8fca3228abf1318f0a4d40b7f4c7fc
soyeon <peach404@naver.com>
2018-11-21 22:29:16 +0900

    [ADD]내장 웹 서버의 이해

--- readme.md
+++ readme.md
@@ -19,13 +19,27 @@ Component 애노테이션이 숨어있으므로 빈으로 등록해준다.
 <img width="516" alt="2018-11-20 1 07 19" src="https://user-images.githubusercontent.com/38067653/48751035-a245dc80-ecc5-11e8-98e5-2fcb68b218c5.png">
 <img width="324" alt="2018-11-20 1 07 54" src="https://user-images.githubusercontent.com/38067653/48751048-affb6200-ecc5-11e8-9215-4caca4af0034.png">
 
-##덮어쓰기를 방지하기
+## 덮어쓰기를 방지하기
 @ConditionalOnMissingBean 을 붙여주면 그 설정 파일을 사용하는 프로젝트에서 설정을 수정했다면 그 수정 파일이 적용된다.
 
 빈재정의로 수정하지 말고 설정 프로젝트에서 별도의 
 Hellotestpropertise 파일 을 생성해서 @ConfigurationProperties 애노테이션을 달고 gettersetter을 달아준다.
 application.properties 파일에 hellotest.name , hellotest.age 값 작성으로 빈 재정의 없이도 변경가능해진다.
 
+
+## 서블릿, 서블릿 컨테이너, 디스페쳐 서블릿, JSP 대한 개념 잡기
+http://mangkyu.tistory.com/14 참고!
+서블릿 컨테이너 - 톰캣
+서블릿 컨테이너는 클라이언트의 요청(Request)을 받아주고 응답(Response)할 수 있게, 웹서버와 소켓을 만들어 통신하며 대표적인 예로 톰캣(Tomcat)이 있습니다.
+톰캣은 실제로 웹서버와 통신하여 JSP(자바 서버 페이지)와 Servlet이 작동하는 환경을 제공해줍니다
+- dispatcherservlet 이란
+http://mangkyu.tistory.com/18 참고!
+
+자동 설정 파일로 내장 웹 서버가 만들어진
+- ServletWebServerFactoryAutoConfiguration- 서블릿 웹 서버 생성
+- DispatcherServletAutoConfiguration - 서블릿 만들고 서블릿 컨테이너에 등록
+
+이렇게 나눠져 있는 이유는 서블릿 컨테이너는 달라질 수 있는데 서블릿은 그렇지 않기 때문이다.
 
 
 
--- src/main/java/me/soyeon/Application.java
+++ src/main/java/me/soyeon/Application.java
@@ -1,26 +1,53 @@
 package me.soyeon;
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.Context;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+    public static void main(String[] args) throws LifecycleException {
+
+        //서블릿 만들기
+        //실제로는 이런 코드를 작성할 일이없다. 왜냐하면 autoconfiguration에 이 과정을 상세하게 설정하고 실행해주는 자동 설정이 존재한다.
+
+
+       Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        Context context = tomcat.addContext("/","/");
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("<html><head><title>");
+                writer.println("Hey Tomcat");
+                writer.println("</title></head>");
+                writer.println("<body><h1>Hello,Tomcat</h1></body>");
+                writer.println("</html>");
+
+            }
+        };
+
+            String servletName = "helloServlet";
+            tomcat.addServlet("/",servletName,servlet);
+            context.addServletMappingDecoded("/hello",servletName);
+
+            tomcat.start();
+            tomcat.getServer().await();
     }
 
-    //빈 재정의 방식으로 수정
-//    @Bean
-//    public Hellotest hellotest(){
-//        Hellotest hellotest = new Hellotest();
-//        hellotest.setName("NEWPUPPY");
-//        hellotest.setAge(10);
-//        return  hellotest;
-//    }
+
 }
